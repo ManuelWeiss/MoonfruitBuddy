c815b50ca4e1e830ab463f1ec58d4c8545e78f77
Graham MacGregor <graham@moonfruits-imac.greekattic.local>
2014-05-01 18:09:12 +0100

    Posting answers to /answers.

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -2,12 +2,48 @@
 
 angular.module('moonBuddyApp')
   .controller('MainCtrl', function ($scope, $http) {
+      $scope.user = {
+          username: "",
+          department: ""
+      };
+
       $http.get('/questions').success(function(res) {
           console.log(res);
           $scope.questions = res;
       });
 
-      $scope.testing = function(e) {
-          console.log(e);
+      $scope.submitAnswers = function(q, a) {
+          //submit to back end;
+          var data = {};
+          if (!$scope.user.username.length || !$scope.user.department.length) { 
+              return this.invalid(); 
+          }
+
+          console.log($scope.user);
+          angular.forEach($scope.questions, function(key, value) {
+              console.log(key, value);
+          });
+
+          data.user_id = $scope.user.username + "@moonfruit.com";
+          data.department = $scope.user.department;
+          data.question_id = q;
+          data.answer = a;
+          
+          console.log('This is what i\'m about to post to /answers : ');
+          console.log(data);
+
+          $http.post('/answers').success(function(res) {
+              console.log(res);
+          }).error(function(res) {
+              console.log(res);
+          });
+
+          return true;
+          
       };
+
+      $scope.invalid = function() {
+          alert('you missed something');
+      };
+
   });
