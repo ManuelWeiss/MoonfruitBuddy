f2d477effccc1cbc3ed762abd21dddc741c2fe43
Manuel Weiss <mweiss@moonfruit.com>
2014-05-02 19:07:15 +0100

    make matching work - work in progress

--- app/models/Answer.scala
+++ app/models/Answer.scala
@@ -7,161 +7,152 @@ import play.api.libs.json._
 import anorm._
 import anorm.SqlParser._
 
+import scala.collection.mutable.HashMap
+
 // Answer represents a single answer
 case class Answer(
-		user_id:     String,
-		question_id: String,
-		answer:      Double = 5.0
-		)
+	user_id: String,
+	question_id: String,
+	answer: Double = 5.0)
 
 object Answer {
 
 	// (de-)serialize to/from Json
-  implicit object AnswerFormat extends Format[Answer] {
-    def reads(json: JsValue) = JsSuccess(Answer(
-      (json \ "user_id").as[String],
-      (json \ "question_id").as[String],
-      (json \ "answer").as[Double]
-    ))
+	implicit object AnswerFormat extends Format[Answer] {
+		def reads(json: JsValue) = JsSuccess(Answer(
+			(json \ "user_id").as[String],
+			(json \ "question_id").as[String],
+			(json \ "answer").as[Double]))
 
-    def writes(q: Answer) = JsObject(Seq(
-      "user_id"     -> JsString(q.user_id),
-      "question_id" -> JsString(q.question_id),
-      "answer"      -> JsNumber(q.answer)
-    ))
-  }
+		def writes(q: Answer) = JsObject(Seq(
+			"user_id" -> JsString(q.user_id),
+			"question_id" -> JsString(q.question_id),
+			"answer" -> JsNumber(q.answer)))
+	}
 
-  /**
-   * Parse an Answer from a SQL ResultSet
-   */
-  val parseAnswerRS = {
-    str("answers.user_id") ~
-    str("answers.question_id") ~
-    get[Double]("answers.answer") map {
-      case user_id ~ question_id ~ answer =>
-      		Answer(user_id, question_id, answer)
-    }
-  }
+	/**
+	 * Parse an Answer from a SQL ResultSet
+	 */
+	val parseAnswerRS = {
+		str("answers.user_id") ~
+			str("answers.question_id") ~
+			get[Double]("answers.answer") map {
+				case user_id ~ question_id ~ answer =>
+					Answer(user_id, question_id, answer)
+			}
+	}
 
-  /**
-   * Get all Answers as a List.
-   */
-  def getAll: List[Answer] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("select * from answers").as(parseAnswerRS *)
-    }
-  }
-  
-  // question_id -> List(answer values)
-  type AnswersForUser = Map[String, List[Double]] 
-		
-  /**
-   * Get all Answers as a Map keyed by user_id.
-   */
-  def getMapByUser = {
-    val answers = Map() ++ getAll.map{case Answer(u, q, a) => (u, (q, a))}
-    answers
-  }
+	/**
+	 * Get all Answers as a List.
+	 */
+	def getAll: List[Answer] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("select * from answers").as(parseAnswerRS *)
+		}
+	}
 
-  /**
-   * Get all answers for a user.
-   */
-  def allForUser(user_id: String): List[Answer] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("select * from answers where user_id = {user_id}").on(
-        	  'user_id     -> user_id
-        	  ).as(parseAnswerRS *)
-    }
-  }
+	/**
+	 * Get all Answers as a Map keyed by user_id.
+	 */
+	def getMapByUser = {
+		val answersByUser = HashMap[String, Map[String, Double]]()
+		getAll.foreach { case Answer(u, q, a) =>
+			val answers = answersByUser.getOrElse(u, Map[String, Double]())
+			answersByUser(u) = answers + (q -> a)
+		}
+		answersByUser
+	}
 
-  /**
-   * Retrieve an answer by user_id & question_id.
-   *
-   * @param user_id The id of the user.
-   * @param question_id The id of the question.
-   */
-  def findById(user_id: String, question_id: String): Option[Answer] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("""
+	/**
+	 * Get all answers for a user.
+	 */
+	def allForUser(user_id: String): List[Answer] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("select * from answers where user_id = {user_id}").on(
+					'user_id -> user_id).as(parseAnswerRS *)
+		}
+	}
+
+	/**
+	 * Retrieve an answer by user_id & question_id.
+	 *
+	 * @param user_id The id of the user.
+	 * @param question_id The id of the question.
+	 */
+	def findById(user_id: String, question_id: String): Option[Answer] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("""
         	select * from answers
         		where user_id = {user_id}
         		and question_id = {question_id}
-        	"""
-        	).on(
-        	  'user_id     -> user_id,
-        	  'question_id -> question_id
-        	  ).as(parseAnswerRS.singleOpt)
-    }
-  }
+        	""").on(
+					'user_id -> user_id,
+					'question_id -> question_id).as(parseAnswerRS.singleOpt)
+		}
+	}
 
-  /**
-   * Update or insert an existing answer.
-   *
-   * @param answer The answer object.
-   */
-  def updateOrInsert(answer: Answer): Option[Answer] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("""
+	/**
+	 * Update or insert an existing answer.
+	 *
+	 * @param answer The answer object.
+	 */
+	def updateOrInsert(answer: Answer): Option[Answer] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("""
 	          merge into answers
 	          (user_id, question_id, answer)
 	          values ( {user_id}, {question_id}, {answer} )
-        	"""
-        	).on(
-        	  'user_id     -> answer.user_id,
-        	  'question_id -> answer.question_id,
-	          'answer      -> answer.answer
-        		).executeUpdate()
-    }
-    Some(answer)
-  }
+        	""").on(
+					'user_id -> answer.user_id,
+					'question_id -> answer.question_id,
+					'answer -> answer.answer).executeUpdate()
+		}
+		Some(answer)
+	}
 
-  /**
-   * Update an existing answer.
-   *
-   * @param answer The answer object.
-   */
-  def update(answer: Answer): Option[Answer] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("""
+	/**
+	 * Update an existing answer.
+	 *
+	 * @param answer The answer object.
+	 */
+	def update(answer: Answer): Option[Answer] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("""
         	update answers set answer = {answer}
         		where user_id = {user_id}
         		and question_id = {question_id}
-        	"""
-        	).on(
-        	  'user_id     -> answer.user_id,
-        	  'question_id -> answer.question_id,
-	          'answer      -> answer.answer
-        		).executeUpdate()
-    }
-    Some(answer)
-  }
+        	""").on(
+					'user_id -> answer.user_id,
+					'question_id -> answer.question_id,
+					'answer -> answer.answer).executeUpdate()
+		}
+		Some(answer)
+	}
 
-  /**
-   * Insert a answer.
-   *
-   * @param answer The Answer object.
-   */
-  def insert(answer: Answer) = {
-    DB.withConnection {
-      implicit connection =>
-        SQL(
-          """
+	/**
+	 * Insert a answer.
+	 *
+	 * @param answer The Answer object.
+	 */
+	def insert(answer: Answer) = {
+		DB.withConnection {
+			implicit connection =>
+				SQL(
+					"""
           insert into answers
           (user_id, question_id, answer)
           values ( {user_id}, {question_id}, {answer} )
-          """
-        ).on(
-        	  'user_id     -> answer.user_id,
-        	  'question_id -> answer.question_id,
-	          'answer      -> answer.answer
-        ).executeUpdate()
-    }
-  }
+          """).on(
+						'user_id -> answer.user_id,
+						'question_id -> answer.question_id,
+						'answer -> answer.answer).executeUpdate()
+		}
+	}
 
 }
 
--- app/models/User.scala
+++ app/models/User.scala
@@ -12,120 +12,123 @@ import models.Answer
 // User represents a user
 // which persists as a SQL table (see conf/evolutions.default/1.sql)
 case class User(
-		id:         String,
-		name:       String,
-		department: String,
-		team:       String //,
-//		answers:    Answers
-		)
+	id: String,
+	name: String,
+	department: String,
+	team: String //,
+	//		answers:    Answers
+	)
 
 object User {
 
-  // (de-)serialize to/from Json
-  implicit object UserFormat extends Format[User] {
-    def reads(json: JsValue) = JsSuccess(User(
-      (json \ "id").as[String],
-      (json \ "name").as[String],
-      (json \ "department").as[String],
-      (json \ "team").as[String]
-    ))
+	// (de-)serialize to/from Json
+	implicit object UserFormat extends Format[User] {
+		def reads(json: JsValue) = JsSuccess(User(
+			(json \ "id").as[String],
+			(json \ "name").as[String],
+			(json \ "department").as[String],
+			(json \ "team").as[String]))
 
-    def writes(user: User) = JsObject(Seq(
-      "id"         -> JsString(user.id),
-      "name"       -> JsString(user.name),
-      "department" -> JsString(user.department),
-      "team"       -> JsString(user.team)
-    ))
-  }
+		def writes(user: User) = JsObject(Seq(
+			"id" -> JsString(user.id),
+			"name" -> JsString(user.name),
+			"department" -> JsString(user.department),
+			"team" -> JsString(user.team)))
+	}
 
-  /**
-   * Parse a User from a SQL ResultSet
-   */
-  val parseRS = {
-    str("users.id") ~
-    str("users.name") ~
-    str("users.department") ~
-    str("users.team") map {
-      case id ~ name ~ department ~ team => User(id, name, department, team)
-    }
-  }
+	/**
+	 * Parse a User from a SQL ResultSet
+	 */
+	val parseRS = {
+		str("users.id") ~
+			str("users.name") ~
+			str("users.department") ~
+			str("users.team") map {
+				case id ~ name ~ department ~ team => User(id, name, department, team)
+			}
+	}
 
-  /**
-   * Get all items. For debugging.
-   */
-  def getAll: List[User] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("select * from users").as(User.parseRS *)
-    }
-  }
+	/**
+	 * Get all items. For debugging.
+	 */
+	def getAll: List[User] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("select * from users").as(User.parseRS *)
+		}
+	}
 
-  /**
-   * Retrieve a user by id.
-   *
-   * @param id The id of the user.
-   */
-  def findById(id: String): Option[User] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("select * from users where id = {id}").on('id -> id).as(User.parseRS.singleOpt)
-    }
-  }
+	/**
+	 * Retrieve a user by id.
+	 *
+	 * @param id The id of the user.
+	 */
+	def findById(id: String): Option[User] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("select * from users where id = {id}").on('id -> id).as(User.parseRS.singleOpt)
+		}
+	}
 
-  /**
-   * Find buddies for a given user
-   *
-   * @param id The id of the user.
-   */
-  def findBuddy(id: String) = {
-	  val answers = Answer.getMapByUser
-	  answers.mkString("\n")
-  }
+	/**
+	 * Find buddies for a given user
+	 *
+	 * @param id The id of the user.
+	 */
+	def findBuddy(id: String) = {
+		val answers = Answer.getMapByUser
+		val thisUser = answers(id) // Map(q_id -> Double)
+		val otherUsers = answers.filterKeys(_ != id)
+//		var lala = Map[???]
+//		otherUsers.keys.foreach{
+//			user_id =>
+//				val otherUser = answers(id)
+//				val pairs = for (q_id <- user.keys) yield (q_id, thisUser(q_id), otherUser(q_id))
+//				lala + (user_id, pairs)
+//		}.mkString("\n")
+		otherUsers
+	}
 
-  /**
-   * Update an existing user.
-   *
-   * @param id The id of the user.
-   */
-  def update(user: User): Option[User] = {
-    DB.withConnection {
-      implicit connection =>
-        SQL("""
+	/**
+	 * Update an existing user.
+	 *
+	 * @param id The id of the user.
+	 */
+	def update(user: User): Option[User] = {
+		DB.withConnection {
+			implicit connection =>
+				SQL("""
         	update users set (name, department, team) = 
         		( {name}, {department}, {team} ) where id = {id}
-        	"""
-        	).on(
-	          'id         -> user.id,
-	          'name       -> user.name,
-	          'department -> user.department,
-	          'team       -> user.team
-        		).executeUpdate()
-    }
-    Some(user)
-  }
+        	""").on(
+					'id -> user.id,
+					'name -> user.name,
+					'department -> user.department,
+					'team -> user.team).executeUpdate()
+		}
+		Some(user)
+	}
 
-  /**
-   * Insert a user.
-   *
-   * @param user The User object.
-   */
-  def insert(user: User) = {
-    DB.withConnection {
-      implicit connection =>
-        SQL(
-          """
+	/**
+	 * Insert a user.
+	 *
+	 * @param user The User object.
+	 */
+	def insert(user: User) = {
+		DB.withConnection {
+			implicit connection =>
+				SQL(
+					"""
           insert into users
           (id, name, department, team)
           values ( {id}, {name}, {department}, {team} )
-          """
-        ).on(
-          'id         -> user.id,
-          'name       -> user.name,
-          'department -> user.department,
-          'team       -> user.team
-        ).executeUpdate()
-    }
-  }
+          """).on(
+						'id -> user.id,
+						'name -> user.name,
+						'department -> user.department,
+						'team -> user.team).executeUpdate()
+		}
+	}
 
 }
 
